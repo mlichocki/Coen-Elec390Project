9ce416a4604ab31fe9da54c528c4b9db99ecb443
mitchelllichocki <mlichocki@me.com>
2017-03-13 21:56:53 -0400

    Fahim's Integration

--- ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/ChildActivity.java
+++ ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/ChildActivity.java
@@ -7,30 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
-
 public class ChildActivity extends AppCompatActivity {
 
-    Button emergency_text;
-    Button emergency_call;
+    Button emergency_text, emergency_call, help;
 
-    ArrayList<String> names = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
 
-        names.add("Mitch");
-        names.add("Felix");
-        names.add("Matt");
-
-        emergency_text= (Button) findViewById(R.id.EM_TEXT);
+        emergency_text = (Button) findViewById(R.id.EM_TEXT);
         emergency_text.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v) {
 
                 Intent textIntent= new Intent (Intent.ACTION_VIEW, Uri.parse ("sms:15149495656"));
-                textIntent.putExtra("sms_body","URGENT: NEED HELP! - VIA TRACKER");
+                textIntent.putExtra("sms_body","URGENT: NEED HELP! - Secure Track");
                 startActivity(textIntent);
             }
 
@@ -38,7 +32,7 @@ public class ChildActivity extends AppCompatActivity {
         });
 
 
-        emergency_call= (Button) findViewById(R.id.EM_CALL);
+        emergency_call = (Button) findViewById(R.id.EM_CALL);
         emergency_call.setOnClickListener (new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -49,11 +43,18 @@ public class ChildActivity extends AppCompatActivity {
             }
         });
 
-    }
+        help = (Button) findViewById(R.id.HelpBtn);
+        help.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
 
-    public void BUTTON(View view){
-        Intent intent = new Intent(this, MapDisplayActivity.class);
-        intent.putExtra("names", names);
-        startActivity(intent);
+                Intent textIntent= new Intent (Intent.ACTION_VIEW, Uri.parse ("sms:15149495656"));
+                textIntent.putExtra("sms_body","URGENT: NEED HELP! - Secure Track");
+                startActivity(textIntent);
+            }
+
+
+        });
+
     }
 }
---
+++ ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/RegisterActivity.java
@@ -0,0 +1,46 @@
+package com.example.mitchelllichocki.elec390project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText username = (EditText)findViewById(R.id.Username);
+        final EditText password = (EditText)findViewById(R.id.Password);
+        final EditText email = (EditText)findViewById(R.id.Email);
+        Button saveButton = (Button)findViewById(R.id.SaveBtn);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+
+                if(username.getText().toString().equals("") || password.getText().toString().equals("") || email.getText().toString().equals("")){
+                    Toast toast = Toast.makeText(getApplicationContext(),"Incomplete Registration",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(),"Credentials Saved",Toast.LENGTH_LONG);
+                    toast.show();
+                    //sharedPreferenceHelper.saveProfile(aProfile);
+                    Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                    intent.putExtra("username",username.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
