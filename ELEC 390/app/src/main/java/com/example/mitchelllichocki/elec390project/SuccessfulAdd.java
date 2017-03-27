b47e0851ebf68152e87894899c14e78ca443c703
mgtimmermans <matthew.timmermans@bell.net>
2017-03-26 20:38:42 -0400

    Create SuccessfulAdd.java

---
+++ ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/SuccessfulAdd.java
@@ -0,0 +1,13 @@
+package com.example.matthew.myapplication;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class SuccessfulAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_add);
+    }
+}
