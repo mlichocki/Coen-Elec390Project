32c268431f24d06ee461b35144d4cd8a62ea3b66
mitchelllichocki <mlichocki@me.com>
2017-04-04 12:36:50 -0400

    The posting of coordinates to the server for the child account is now done by a service, which keeps posting their coordinates even if the child stops the application.

--- ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/LoginActivity.java
+++ ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/LoginActivity.java
@@ -1,14 +1,13 @@
 package com.example.mitchelllichocki.elec390project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -44,33 +43,8 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
-
-                if(true){
-                    BackgroundWorker backgroundWorker = new BackgroundWorker(LoginActivity.this);
-                    backgroundWorker.login(username.getText().toString(), password.getText().toString());
-                }
-                else {
-                    if (username.getText().toString().equals(childUsername)) {
-                        if (password.getText().toString().equals(childPassword)) {
-                            Intent intent = new Intent(LoginActivity.this, ChildActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast toast1 = Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_LONG);
-                            toast1.show();
-                        }
-                    } else if (username.getText().toString().equals(guardianUsername)) {
-                        if (password.getText().toString().equals(guardianPassword)) {
-                            Intent intent = new Intent(LoginActivity.this, GuardianActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast toast1 = Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_LONG);
-                            toast1.show();
-                        }
-                    } else {
-                        Toast toast1 = Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_LONG);
-                        toast1.show();
-                    }
-                }
+                BackgroundWorker backgroundWorker = new BackgroundWorker(LoginActivity.this);
+                backgroundWorker.login(username.getText().toString(), password.getText().toString());
 
             }
 
