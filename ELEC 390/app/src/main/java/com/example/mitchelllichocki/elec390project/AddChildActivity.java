bb3cf97b61c6f0396184e8b07d1f95cf5217caad
mitchelllichocki <mlichocki@me.com>
2017-04-03 20:29:19 -0400

    AddChildActivity is finalized.

--- ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/AddChildActivity.java
+++ ELEC 390/app/src/main/java/com/example/mitchelllichocki/elec390project/AddChildActivity.java
@@ -1,44 +1,47 @@
 package com.example.mitchelllichocki.elec390project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.Button;
-import android.content.Intent;
+
+import java.util.ArrayList;
 
 public class AddChildActivity extends AppCompatActivity {
 
     //Creation of edittext and button
 
     Button usrbtn;
-    EditText usr,psd;
+    EditText usr,psd, name;
+    String guardianUsername;
+    ArrayList<String> children;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_child_activity);
 
+        Intent intent = getIntent();
+        children = intent.getStringArrayListExtra("children");
+        guardianUsername = intent.getStringExtra("guardianUsername");
+
         usr = (EditText) findViewById(R.id.usr);
         psd = (EditText) findViewById(R.id.psd);
+        name = (EditText) findViewById(R.id.name);
         usrbtn = (Button) findViewById(R.id.usrbtn);
-
+        final BackgroundWorker backgroundWorker = new BackgroundWorker(this);
         usrbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v){
 
                 //IF STATEMENT FOR PROPER CREDENTIALS
-                if (usr.getText().toString().equals("Child") && psd.getText().toString().equals("Child")){
-                Toast.makeText(getApplicationContext(),"Child Successfully Added", Toast.LENGTH_LONG).show();
-                Intent myintent = new Intent (AddChildActivity.this, SuccessfulAdd.class);
-                startActivity(myintent);
+                if ( !((usr.getText().toString().equals("") || psd.getText().toString().equals("")) || name.getText().toString().equals("")) ){
+                    Toast.makeText(getApplicationContext(),"Child Successfully Added", Toast.LENGTH_LONG).show();
+                    backgroundWorker.addChild(guardianUsername, usr.getText().toString(), name.getText().toString(), psd.getText().toString(), children);
                 }
-
-                // ELSE STATEMENT FOR BLANK TEXT FIELDS
-                else if (usr.getText().toString().equals("") || psd.getText().toString().equals("")){
-                Toast.makeText(getApplicationContext(),"Field(s) left blank!", Toast.LENGTH_LONG).show();
-              }
             }
         });
     }
